eada7445ef4e4d985f7a56d3127f3977592f4481
Michal Lopuszynski <lopusz@users.noreply.github.com>
2014-10-22 15:45:33 +0200

    Adding exact implemmentation of ApproximateStringFrequencyCounter.

--- src/main/clojure/hashlab/frequency.clj
+++ src/main/clojure/hashlab/frequency.clj
@@ -2,12 +2,9 @@
   (:refer-clojure :exclude [assert get merge])
   (:require
      [ pjstadig.assertions :refer [assert]])
-  (:import [com.clearspring.analytics.stream.frequency CountMinSketch]))
-
-;; From CountMinSketch sources:
-;; The estimate is correct within 'epsilon' * (total item count),
-;; * with probability 'confidence'.
-
+  (:import
+     [java.util AbstractMap HashMap ]
+     [com.clearspring.analytics.stream.frequency CountMinSketch]))
 
 (defprotocol ApproxStringFrequencyCounter
   (add-str! [ asfc ^String s]
@@ -15,7 +12,32 @@
   (approx-count-str [asfc ^String s])
   (merge-internal [first-asfc rest-asfcs]))
 
+(defn ^:private merge-to-first [ ^AbstractMap m1 ^AbstractMap m2]
+  (dorun
+    (map
+      #(add-str! ^AbstractMap m1 % (.get m2 %))
+      (.keySet m2))))
+
 (extend-protocol ApproxStringFrequencyCounter
+  AbstractMap
+  (add-str!
+    ([ a-map ^String s ^Long count ]
+       (if-let [val (.get a-map s)]
+         (.put a-map s (+ val count))
+         (.put a-map s count))
+       a-map)
+    ([a-map ^String s]
+      (add-str! a-map s 1)))
+  (approx-count-str [ a-map ^String s]
+     (if-let [ c (.get a-map s) ]
+       c
+       0))
+  (merge-internal [first-map rest-maps]
+    (let [res (.clone first-map) ]
+      (dorun
+        (map #(merge-to-first res %) rest-maps))
+      res))
+
   CountMinSketch
   (add-str!
     ([ cms ^String s]
@@ -35,7 +57,12 @@
 (defn merge [ & args ]
   (merge-internal (first args) (rest args)))
 
-(defn create-count-min-sketch [ ^double epsOfTotalCount
-                                ^double confidence
-                                ^Integer seed]
+(defn create-count-min-sketch
+  "From CountMinSketch sources:
+   The estimate is correct within 'epsOfTotalCount' * (total item count),
+   with probability 'confidence'."
+  [ ^double epsOfTotalCount ^double confidence ^Integer seed]
   (CountMinSketch.  epsOfTotalCount confidence seed))
+
+(defn create-hash-map []
+  (HashMap.))
